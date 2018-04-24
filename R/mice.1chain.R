fa77cebefe78f22f13676526571a720850288e28
alexanderrobitzsch <robitzsch@ipn.uni-kiel.de>
2018-04-24 15:39:15 +0200

    2.11-12

--- R/mice.1chain.R
+++ R/mice.1chain.R
@@ -1,5 +1,5 @@
 ## File Name: mice.1chain.R
-## File Version: 0.35
+## File Version: 0.46
 
 ###############################################################################
 ###############################################################################
@@ -13,14 +13,22 @@ mice.1chain <- function(data, burnin = 10 , iter = 20 , Nimp = 10 ,
 		defaultMethod = c("pmm", "logreg", "polyreg", "polr"),
 		diagnostics = TRUE, printFlag = TRUE,
 		seed = NA, imputationMethod = NULL,
-		defaultImputationMethod = NULL, data.init = NULL, ...){
-    ###################################################################------
+		defaultImputationMethod = NULL, data.init = NULL, ...)
+{
 	CALL <- match.call()
-    # burnin phase
-    iterstep0 <- round( seq( burnin , iter , length = Nimp+1 ) )
-    iterstep <- diff( iterstep0  )
+	
+	#-- check input
+	res <- mice_1chain_check_input( burnin=burnin, iter=iter, Nimp=Nimp ) 
+	burnin <- res$burnin
+	iter <- res$iter
+	Nimp <- res$Nimp
+	
+	#-- determine burnin phase
+	iterstep0 <- round( seq( burnin , iter , length = Nimp+1 ) )
+	iterstep <- diff( iterstep0  )
 	implist <- list(  1:(Nimp+1) )
 	datlist <- list( 1:Nimp )
+
 	# indicator for factors
 	VV <- ncol(data)
 	ind.factor <- sapply( 1:VV , FUN = function(vv){ 
@@ -32,30 +40,30 @@ mice.1chain <- function(data, burnin = 10 , iter = 20 , Nimp = 10 ,
 	NMiss <- colSums( is.na(data[,ind.num]))		
 
 	#******************************
-    cat("************ BURNIN PHASE |", "Iterations 1 -" , burnin , 
+	cat("************ BURNIN PHASE |", "Iterations 1 -" , burnin , 
 			"******************\n") 
-			utils::flush.console()
-    implist[[1]] <- imp0 <- mice::mice( data , maxit=burnin , m=1 , 
-                imputationMethod = imputationMethod ,
-                predictorMatrix = predictorMatrix , method=method , 
+	utils::flush.console()
+	implist[[1]] <- imp0 <- mice::mice( data , maxit=burnin , m=1 , 
+				imputationMethod = imputationMethod ,
+				predictorMatrix = predictorMatrix , method=method , 
 				visitSequence=visitSequence ,form=form , post=post , 
 				defaultMethod=defaultMethod ,  diagnostics=diagnostics ,
-                printFlag=printFlag , seed=seed , 
+				printFlag=printFlag , seed=seed , 
 				defaultImputationMethod=defaultImputationMethod ,
-                data.init = data.init , ... )
-    dat0 <- mice::complete( imp0 , 1 )
+				data.init = data.init , ... )
+	dat0 <- mice::complete( imp0 , 1 )
 	chainMean <- t( imp0$chainMean[,,1] )
 	chainVar <- t( imp0$chainVar[,,1] )
 	rownames(chainVar)[ nrow(chainVar) ] <- 
 		rownames(chainMean)[ nrow(chainMean) ] <- "end_burnin"
-    # imputation phase
+	# imputation phase
 	
-    cat("\n\n************ IMPUTATION PHASE |", "Iterations", burnin+1 , "-" , 
+	cat("\n\n************ IMPUTATION PHASE |", "Iterations", burnin+1 , "-" , 
 				iter , "******************\n") ; 
 	utils::flush.console()
-    for ( mm in 1:Nimp){
-	    cat("\n --- Imputation" , mm , "| Iterations" ,
-			iterstep0[mm] , "-" , iterstep0[mm+1] , "\n" ) ; 
+	for ( mm in 1:Nimp){
+		cat("\n --- Imputation" , mm , "| Iterations" ,
+		iterstep0[mm] + 1 , "-" , iterstep0[mm+1] , "\n" )
 		utils::flush.console()
 		implist[[mm+1]] <- imp1 <- mice::mice( data , maxit=iterstep[mm] , m=1 , 
 					imputationMethod = imputationMethod ,
@@ -70,14 +78,12 @@ mice.1chain <- function(data, burnin = 10 , iter = 20 , Nimp = 10 ,
 			rownames(chainMean)[ nrow(chainMean) ] <- 
 				paste0( "imp_" , mm )	
 	}
-        
-    ###################################################--------------------------
-    # post-processing
 
+	#------ post-processing
 	# conversion into a mids object
 	ids <- seq( 1 , nrow(data) )
 	datalong <- data.frame( ".imp"=0 , "id"= ids , data )
-    for ( mm in 1:Nimp ){ 
+	for ( mm in 1:Nimp ){ 
 		datalong <- rbind( datalong , 
 						data.frame( ".imp"=mm , "id" = ids , datlist[[mm]] ) )
 	}
@@ -99,17 +105,13 @@ mice.1chain <- function(data, burnin = 10 , iter = 20 , Nimp = 10 ,
 	VarObs[ is.na(VarObs) ] <- 0	
 	chainVarPar <- ( chainVar * matrix( (NMiss[vars]) , nrow=cM , ncol=length(vars) , byrow=TRUE ) +
 				matrix( ( NObs[vars])* VarObs[vars] , nrow=cM , ncol=length(vars) , byrow=TRUE ) ) /
-						( eps+ matrix( NObs[vars] + NMiss[vars] , nrow=cM , ncol=length(vars) , byrow=TRUE ) )						
+						( eps+ matrix( NObs[vars] + NMiss[vars] , nrow=cM , ncol=length(vars) , byrow=TRUE ) )
 	midsobj <- mice::as.mids(datalong , .imp=1)
-#	midsobj$m <- Nimp
-	
 	imm <- implist[[mm+1]]
 	midsobj$predictorMatrix <- imm$predictorMatrix
 	midsobj$method <- imm$method
-#	midsobj$m <- Nimp
-	
 	midsobj$call <- CALL
-	
+
 	res <- list( "midsobj"=midsobj , "datlist"=datlist , "datalong" = datalong , 
 					"implist" = implist ,
 					 "chainMPar"=chainMPar , 
@@ -119,25 +121,26 @@ mice.1chain <- function(data, burnin = 10 , iter = 20 , Nimp = 10 ,
 					"time" = Sys.time()
 					)
 	res$nobs <- nrow(data)
-	res$nvars <- ncol(data)					
+	res$nvars <- ncol(data)
 	class(res) <- "mids.1chain"
-    return(res)
+	return(res)
 }
 ###############################################################################
 ###############################################################################
 ###############################################################################
 # S3 method summary
-summary.mids.1chain <- function( object , ... ){
+summary.mids.1chain <- function( object , ... )
+{
    cat("Multiply imputed dataset using one chain\n\n")
-#   cat("***" , paste0("Date : " , Sys.time() ) , "\n\n")
    cat( paste0( "Number of iterations = " , object$iter) , "\n")
    cat( paste0( "Number of burnin-iterations = " , object$burnin) , "\n")   
    cat( paste0( "Number of imputations = " , object$Nimp) , "\n")   
    cat( paste0( object$nobs , " cases and " , object$nvars , " variables \n\n" ) )   
    cat("---------\n")   
    print( summary( object$midsobj ) )
-	}
-# print method	
+}
+
+#--- print method	
 print.mids.1chain <- function( x , ...){
 	summary.mids.1chain( object = x , ...)
 	}	
---
+++ R/mice_1chain_check_input.R
@@ -0,0 +1,15 @@
+## File Name: mice_1chain_check_input.R
+## File Version: 0.01
+
+mice_1chain_check_input <- function(burnin, iter, Nimp)
+{
+	if (burnin <= 0){
+		burnin <- 1
+	}
+	if (iter <= burnin + Nimp ){
+		iter <- burnin + Nimp
+	}	
+	#--- output
+	res <- list( burnin=burnin, iter=iter, Nimp=Nimp)
+	return(res)
+}
