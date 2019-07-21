b71b9aef8aff4457bc0d31defae19dbe7866400f
alexanderrobitzsch <robitzsch@ipn.uni-kiel.de>
2019-07-21 19:40:23 +0200

    3.5-12

--- R/pool.mids.nmi.R
+++ R/pool.mids.nmi.R
@@ -1,62 +1,59 @@
 ## File Name: pool.mids.nmi.R
-## File Version: 0.16
+## File Version: 0.17
 
-###################################################
-# pooling function for nested multiple imputation objects
-pool.mids.nmi <- function( object, method="largesample" ){
 
-        call <- match.call()
-        Nimp <- object$Nimp
-        NB <- Nimp["between"]
-        NW <- Nimp["within"]
-        anal <- object$analyses
+#**** pooling function for nested multiple imputation objects
+pool.mids.nmi <- function( object, method="largesample" )
+{
+    call <- match.call()
+    Nimp <- object$Nimp
+    NB <- Nimp["between"]
+    NW <- Nimp["within"]
+    anal <- object$analyses
+    pool_results <- as.list(1:NB)
+    for (bb in 1:NB){
+        anal.bb <- anal[[bb]]
+        anal2 <- list( "analyses"=anal.bb )
+        class(anal2) <- "mira"
+        dfcom <- 999999
+        pool_results[[bb]] <- mice::pool( anal2, dfcom=dfcom)
+    }
 
-        pool_results <- as.list(1:NB)
-        for (bb in 1:NB){
-        #    bb <- 1
-            anal.bb <- anal[[bb]]
-            anal2 <- list( "analyses"=anal.bb )
-            class(anal2) <- "mira"
-            pool_results[[bb]] <- mice::pool( anal2, method=method)
-        }
+    pool1 <- pool_results[[1]]$u[1,,]
+    NV <- nrow(pool1)
+    cn <- colnames(pool)
+    v1 <- vcov( object$analyses[[1]][[1]] )
+    if ( ! is.null( rownames(v1) ) ){
+        cn <- rownames(v1)
+    }
 
-        pool1 <- pool_results[[1]]$u[1,,]
+    # collect parameter estimates
+    qhat <- array( NA, dim=c( NB, NW, NV ) )
+    dimnames(qhat)[[3]] <- cn
+    dimnames(qhat)[[1]] <- paste0("Between_Imp", 1:NB )
+    dimnames(qhat)[[2]] <- paste0("Within_Imp", 1:NW )
+    for (bb in 1:NB){
+        qhat[bb,,] <- pool_results[[bb]]$qhat
+    }
 
-        NV <- nrow(pool1)
-        cn <- colnames(pool)
-        v1 <- vcov( object$analyses[[1]][[1]] )
-        if ( ! is.null( rownames(v1) ) ){
-            cn <- rownames(v1)
-        }
+    # collect estimated variance matrices
+    u <- array( NA, dim=c( NB, NW, NV, NV) )
+    dimnames(u)[[4]] <- dimnames(u)[[3]] <- cn
+    dimnames(u)[[1]] <- paste0("Between_Imp", 1:NB )
+    dimnames(u)[[2]] <- paste0("Within_Imp", 1:NW )
 
-        # collect parameter estimates
-        qhat <- array( NA, dim=c( NB, NW, NV ) )
-        dimnames(qhat)[[3]] <- cn
-        dimnames(qhat)[[1]] <- paste0("Between_Imp", 1:NB )
-        dimnames(qhat)[[2]] <- paste0("Within_Imp", 1:NW )
-        for (bb in 1:NB){
-            qhat[bb,,] <- pool_results[[bb]]$qhat
-        }
+    for (bb in 1:NB){
+        u[bb,,,] <- pool_results[[bb]]$u
+    }
 
-        # collect estimated variance matrices
-        u <- array( NA, dim=c( NB, NW, NV, NV) )
-        dimnames(u)[[4]] <- dimnames(u)[[3]] <- cn
-        dimnames(u)[[1]] <- paste0("Between_Imp", 1:NB )
-        dimnames(u)[[2]] <- paste0("Within_Imp", 1:NW )
-
-        for (bb in 1:NB){
-            u[bb,,,] <- pool_results[[bb]]$u
-        }
-
-        #*************************
-        # summary statistics
-        fit <- pool.nmi.scalar.helper( qhat, u, NV, NB, NW )
-        fit$Nimp <- Nimp
-        class(fit) <- "mipo.nmi"
-        return(fit)
+    #-- summary statistics
+    fit <- pool.nmi.scalar.helper( qhat, u, NV, NB, NW )
+    fit$Nimp <- Nimp
+    class(fit) <- "mipo.nmi"
+    return(fit)
 }
-###########################################################################
-# coef and vcov method
+
+#**** coef and vcov method
 coef.mipo.nmi <- function( object, ... ){
     return(object$qbar)
 }
--- README.md
+++ README.md
@@ -22,9 +22,9 @@ The CRAN version can be installed from within R using:
 utils::install.packages("miceadds")
 ```
 
-#### GitHub version `miceadds` 3.5-11 (2019-07-04)
+#### GitHub version `miceadds` 3.5-12 (2019-07-21)
 
-[![](https://img.shields.io/badge/github%20version-3.5--11-orange.svg)](https://github.com/alexanderrobitzsch/miceadds)&#160;&#160;
+[![](https://img.shields.io/badge/github%20version-3.5--12-orange.svg)](https://github.com/alexanderrobitzsch/miceadds)&#160;&#160;
 
 The version hosted [here](https://github.com/alexanderrobitzsch/miceadds) is the development version of `miceadds`. 
 The GitHub version can be installed using `devtools` as:
